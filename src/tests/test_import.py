d5ff2a86ba6ec560b60a1ef39dd39fbe6ab0030f
Martin Lycko <martin@mlycko.com>
2024-02-14 15:44:54 +0000

    Started writing test for the implementation

---
+++ src/tests/test_import.py
@@ -0,0 +1,27 @@
+def test_number_of_events():
+    # Use the running-example.csv file in the sample_data folder sourced from https://processmining.org/old-version/event-book.html
+    # Create a new eventlog data structure based on an imported event log
+    # Extract the number of cases with something like events.number()
+    # Should return 42
+    return False
+
+def test_number_of_cases():
+    # Use the running-example.csv file in the sample_data folder sourced from https://processmining.org/old-version/event-book.html
+    # Create a new eventlog data structure based on an imported event log
+    # Extract the number of cases with something like cases.number()
+    # Should return 6
+    return False
+
+def test_number_of_activities():
+    # Use the running-example.csv file in the sample_data folder sourced from https://processmining.org/old-version/event-book.html
+    # Create a new eventlog data structure based on an imported event log
+    # Extract the number of cases with something like activities.number()
+    # Should return 8
+    return False
+
+def test_number_of_resources():
+    # Use the running-example.csv file in the sample_data folder sourced from https://processmining.org/old-version/event-book.html
+    # Create a new eventlog data structure based on an imported event log
+    # Extract the number of cases with something like resources.number()
+    # Should return 6
+    return False

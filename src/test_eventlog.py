c7b2f8b3a6a2fa647fe183faf2eaea955056eaea
Martin Lycko <martin@mlycko.com>
2024-03-08 14:42:22 +0000

    Cleanup of to be completed tests for cases

--- src/test_eventlog.py
+++ src/test_eventlog.py
@@ -25,24 +25,24 @@ if __name__ == "__main__":
     assert elog.resources.count == 6
 
     # To be converted into a proper test - may not always be returned in same order, sample cases by ID
-    case=elog.cases.case_list[1].case_details()
-    print(case["path"][0].time_end)
-    print(case["path"][1].time_end)
-    print(case["path"][2].time_end)
-    print(case["path"][3].time_end)
-    print(case["path"][4].time_end)
+    # case=elog.cases.case_list[1].case_details()
+    # print(case["path"][0].time_end)
+    # print(case["path"][1].time_end)
+    # print(case["path"][2].time_end)
+    # print(case["path"][3].time_end)
+    # print(case["path"][4].time_end)
 
     # Convert this into a test - should be 9 days 33 mins for case with internal ID 1
-    print(elog.cases.case_list[1].turnaround_time())
+    # print(elog.cases.case_list[1].turnaround_time())
 
     # Tests the print function of the case class
-    print(elog.cases.case_list[1])
+    # print(elog.cases.case_list[1])
 
     # Print turnarount times by case number  
-    for caseID, turnaroundtime in elog.cases.turnaround_times().items():
-        print(caseID, turnaroundtime)
+    # for caseID, turnaroundtime in elog.cases.turnaround_times().items():
+    #     print(caseID, turnaroundtime)
 
     # Test turnaround stats
-    print(elog.cases.case_with_min_turnaround_time())
-    print(elog.cases.case_with_max_turnaround_time())
-    print(elog.cases.avg_turnaround_time())
+    # print(elog.cases.case_with_min_turnaround_time())
+    # print(elog.cases.case_with_max_turnaround_time())
+    # print(elog.cases.avg_turnaround_time())

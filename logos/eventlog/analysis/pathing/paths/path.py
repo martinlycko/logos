e543ba6294a02a2c05e61bb1615d235f7e5e5ffa
Martin Lycko <martin@mlycko.com>
2024-06-13 14:57:33 +0100

    Restructuring of code base for more path analysis

---
+++ logos/eventlog/analysis/pathing/paths/path.py
@@ -0,0 +1,14 @@
+# For type safety and code quality
+from pydantic import BaseModel, NonNegativeInt
+from typing import List
+
+# From other modules
+from ....elements.case import Case
+from ....elements.activity import Activity
+
+
+class Path(BaseModel):
+    # A class to capture a single oath
+    id: NonNegativeInt               # Generated, position in cases
+    activities: List[Activity] = []  # List of activities in order for path
+    cases: List[Case] = []           # List of cases that follow this path
--- logos/eventlog/analysis/pathing/pathtree/pathnode.py
+++ logos/eventlog/analysis/pathing/pathtree/pathnode.py
@@ -3,12 +3,14 @@ from pydantic import BaseModel, NonNegativeInt
 from typing import List
 
 # From other modules
-from ...elements.case import Case
-from ...elements.activity import Activity
+from ....elements.case import Case
+from ....elements.activity import Activity
 
 
-class Path(BaseModel):
-    # A class to capture a single oath
+class PathNode(BaseModel):
+    # A class to capture a node in a tree structure forming process paths
+    # Each path node reflects an activity and points and subsequent activities
+    # Also captured the cases that follow from this node to subsequent nodes
     id: NonNegativeInt               # Generated, position in cases
     activities: List[Activity] = []  # List of activities in order for path
     cases: List[Case] = []           # List of cases that follow this path
